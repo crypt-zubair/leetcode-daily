a1d7218d5f54d92b8e21c86a42816913a51b646e
ZUBAIR AHMED <zub.airrr.mail@gmail.com>
2026-07-09 19:30:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (22.42%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,18 @@
+bool isPalindrome(char* s) {
+    int l=0;
+    int r=strlen(s)-1;
+    while(l<r){
+        if(!isalnum (s[l])){
+            l++;
+            continue;
+        }if(!isalnum (s[r])){
+            r--;
+            continue;
+        }
+    if (tolower (s[l]) != tolower (s[r])){
+        return false;
+    }else {
+        l++;
+        --r;
+    }
+}return true;}
