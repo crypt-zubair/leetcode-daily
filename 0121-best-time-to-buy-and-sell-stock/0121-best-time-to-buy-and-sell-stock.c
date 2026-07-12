b39f187a8c75102965648a00d88e155dd4521109
ZUBAIR AHMED <zub.airrr.mail@gmail.com>
2026-07-12 22:10:27 +0530

    Time: 0 ms (100%), Space: 16 MB (87.23%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -2,6 +2,9 @@ int maxProfit(int* prices, int pricesSize) {
     int buy=prices[0];
     int profit=0;
     int cp=0;
+    if(pricesSize<=1){
+        return 0;
+    }
     for(int i=0;i< pricesSize;i++){
         if(prices[i] < buy) {
            buy = prices[i] ;
