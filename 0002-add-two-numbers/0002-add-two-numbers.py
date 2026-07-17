9fb73340a7b35eb702b46e5d7481d2a27efe06fb
ZUBAIR AHMED <zub.airrr.mail@gmail.com>
2026-07-17 23:02:07 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.62%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,22 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(0)
-        current=head
-        carry=0
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            total = val1 + val2 + carry
+        ListNode = type(l1)
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        while l1 and l2:
+            total = l1.val + l2.val + carry
             carry = total // 10
-            digit = total % 10
-            current.next = ListNode(digit)
-            current = current.next
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            total = l1.val + carry
+            carry = total // 10
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            l1 = l1.next
+        while l2:
+            total = l2.val + carry
+            carry = total // 10
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            l2 = l2.next
+        if carry:
+            curr.next = ListNode(carry)
             
-        return head.next
+        return dummy.next
