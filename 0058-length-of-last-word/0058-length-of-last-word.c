5ee5752acc8aeee6ecc8d813f614a0f75d156de6
ZUBAIR AHMED <zub.airrr.mail@gmail.com>
2026-07-28 22:31:09 +0530

    Time: 0 ms (100%), Space: 8.9 MB (16.52%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,12 @@
+int lengthOfLastWord(char* s) {
+    int i=strlen(s)-1;
+   int length=0;
+   while(i>=0 && s[i]==' '){
+    i--;
+   }
+   while(i>=0 && s[i]!=' ')
+   {
+    i--;
+    length++;
+      }return length;
+}
